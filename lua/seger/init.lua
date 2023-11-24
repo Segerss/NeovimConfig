300f097b555249c9ce4248e2a92b5207280c1d7f
Seger Sars <segersars@gmail.com>
2023-11-24 21:22:30 +0100

    added some more remaps

--- lua/seger/init.lua
+++ lua/seger/init.lua
@@ -1,3 +1,4 @@
 require("seger.packer")
 require("seger.remap")
+require("seger.set")
 print("hello from the seger")
--- lua/seger/remap.lua
+++ lua/seger/remap.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+vim.keymap.set("n", "J", "5j")
+vim.keymap.set("n", "K", "5k")
 
 vim.keymap.set("i", "jk", "<Esc>")
