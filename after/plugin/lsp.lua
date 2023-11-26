0a8200692883610de4c5afd46468aea42cc093f0
Seger Sars <segersars@gmail.com>
2023-11-26 13:34:17 +0100

    changed theme and added function signatures while inserting and a integrated terminal

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -26,6 +26,29 @@ require('mason-lspconfig').setup({
 		end
 	},
 })
+-- local base = require('lspconfig')
+-- local on_attach = base.on_attach
+
+
+require('lspconfig').lua_ls.setup({
+settings = {
+    Lua = {
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+    },
+  },
+})
+-- require('lspconfig').clangd.setup({
+ -- on_attach = function(client, bufnr)
+	-- client.server_capabilities.signatureHelpProvider = false
+	-- on_attach(client, bufnr);
+ -- end;
+-- })
+require('lspconfig').clangd.setup({})
+require('lspconfig').pylsp.setup({})
+require('lspconfig').jsonls.setup({})
 
 -- Fix Undefined global 'vim'
 -- lsp.nvim_workspace()
@@ -62,7 +85,7 @@ lsp.on_attach(function(client, bufnr)
 	local opts = {buffer = bufnr, remap = false}
 
 	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+-- 	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
 	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
 	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
--- lua/seger/packer.lua
+++ lua/seger/packer.lua
@@ -5,52 +5,63 @@ print("print from packer.lua")
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use('wbthomason/packer.nvim')
+    -- Packer can manage itself
+    use('wbthomason/packer.nvim')
 
-	use({
-		'nvim-telescope/telescope.nvim', tag = '0.1.4',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	})
+    use({
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    })
 
-	use({ 'rose-pine/neovim', as = 'rose-pine',
-	config = function() 
-		vim.cmd('colorscheme rose-pine')
-	end})
+    use({ 'rose-pine/neovim', as = 'rose-pine',
+    config = function() 
+        vim.cmd('colorscheme rose-pine')
+    end})
 
-	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-	use( 'nvim-treesitter/playground')
-	use('ThePrimeagen/harpoon')
-	use('mbbill/undotree')
-	use('tpope/vim-fugitive')
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use( 'nvim-treesitter/playground')
+    use('ThePrimeagen/harpoon')
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
 
-	-- use {
-		-- "williamboman/mason.nvim",
-		-- "williamboman/mason-lspconfig.nvim",
-		-- "neovim/nvim-lspconfig",
-	-- }
+    -- use {
+        -- "williamboman/mason.nvim",
+        -- "williamboman/mason-lspconfig.nvim",
+        -- "neovim/nvim-lspconfig",
+        -- }
 
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
-		requires = {
-			--- Uncomment these if you want to manage LSP servers from neovim
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
+        use {
+            'VonHeikemen/lsp-zero.nvim',
+            branch = 'v3.x',
+            requires = {
+                --- Uncomment these if you want to manage LSP servers from neovim
+                {'williamboman/mason.nvim'},
+                {'williamboman/mason-lspconfig.nvim'},
 
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-buffer'},
-			{'hrsh7th/cmp-path'},
-			{'L3MON4D3/LuaSnip'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'hrsh7th/cmp-nvim-lua'},
+                -- LSP Support
+                {'neovim/nvim-lspconfig'},
+                -- Autocompletion
+                {'hrsh7th/nvim-cmp'},
+                {'hrsh7th/cmp-buffer'},
+                {'hrsh7th/cmp-path'},
+                {'L3MON4D3/LuaSnip'},
+                {'hrsh7th/cmp-nvim-lsp'},
+                {'hrsh7th/cmp-nvim-lua'},
 
-			{'rafamadriz/friendly-snippets'},
-		}
-	}
-end
-)
+                {'rafamadriz/friendly-snippets'},
+            }
+        }
+
+        use {
+            "ray-x/lsp_signature.nvim",
+        }
+
+        use {
+            "akinsho/toggleterm.nvim",
+        }
+
+        use { "catppuccin/nvim", as = "catppuccin" }
+
+    end
+    )
